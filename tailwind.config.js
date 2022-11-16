6980c37fbab522a3de49b095651992029b293164
wumbles <77227394+wumbles@users.noreply.github.com>
2022-11-16 00:59:06 -0700

    added tailwind and daisyui; general setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: true,
+  },
+};
