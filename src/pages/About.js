2e02576cfb89b5612f943feecfa7ca1ce1fe7da9
wumbles <77227394+wumbles@users.noreply.github.com>
2022-11-21 02:11:07 -0700

    added pages

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        This project is a React app that can be used to search GitHub profiles
+        and see profile details.
+      </p>
+    </>
+  );
+};
+
+export default About;
