ec4fc8437a65db5633f14d443db48283c86a2617
wumbles <77227394+wumbles@users.noreply.github.com>
2022-11-26 20:00:31 -0700

    user logins can now be fetched

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
+import UserResults from "../components/users/UserResults";
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome</h1>
-    </div>
+    <>
+      {/* SEARCH COMPONENT */}
+      <UserResults />
+    </>
   );
 };
 
