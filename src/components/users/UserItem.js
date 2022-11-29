8a7bcabc3cd9e7cd48eb775c11381e0569bb50f1
wumbles <77227394+wumbles@users.noreply.github.com>
2022-11-28 19:20:05 -0700

    added styling and links to user profiles

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,33 @@
-const UserItem = () => {
-  return <div>UserItem</div>;
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const UserItem = ({ user: { login, avatar_url } }) => {
+  return (
+    <div className="card shadow-md compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="Profile Picture" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+          <Link
+            className="text-base-content text-opacity-40"
+            to={`/user/${login}`}
+          >
+            <h4 className="text-sky-600">Visit Profile</h4>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
 };
 
 export default UserItem;
--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import UserItem from "../users/UserItem";
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
@@ -25,7 +26,7 @@ const UserResults = () => {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
