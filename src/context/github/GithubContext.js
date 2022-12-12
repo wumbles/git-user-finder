6af580840694513893e90e1a97ea49d423102e5d
wumbles <77227394+wumbles@users.noreply.github.com>
2022-12-11 17:05:41 -0700

    can now clear users after search

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -34,12 +34,20 @@ export const GithubProvider = ({ children }) => {
     dispatch({ type: "GET_USERS", payload: items });
   };
 
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+
   // Set loading
   const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
