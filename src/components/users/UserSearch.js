c71b8ef4cafef384873d74ca53fa1a4fd0df8f2e
wumbles <77227394+wumbles@users.noreply.github.com>
2022-12-14 15:35:33 -0700

    added alert if no text is input in the search box

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -1,10 +1,12 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 const UserSearch = () => {
   const [text, setText] = useState("");
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
 
@@ -12,7 +14,7 @@ const UserSearch = () => {
     e.preventDefault();
 
     if (text === "") {
-      alert("Please type something.");
+      setAlert("Please search for a profile.", "error");
     } else {
       searchUsers(text);
 
